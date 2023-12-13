a65f45881bfd3f361dc3ff37ffbe955986b1d1ff
Dmonterov <145517235+Dmonterov@users.noreply.github.com>
2023-12-13 16:38:47 -0600

    Update SmartHomeTesteo.py

--- SmartHomeTesteo.py
+++ SmartHomeTesteo.py
@@ -45,3 +45,34 @@ def guardarUsuarios(usuarios):
     with open("usuarios.txt", "a+") as file: 
         for usuario in usuarios: # cada dicionmario representa un usuario, for diccionario in usuarios
             file.write(f"{usuario['Nombre']},{usuario['Correo']},{usuario['PIN']
+
+def consultarInfoPorPIN():
+    pinConsulta = input("Ingrese su PIN para consultar la información: ")
+    nombreConsulta = input("Ingrese su nombre para consultar la información: ")
+    usuarios = cargarUsuarios()
+
+    for usuario in usuarios:
+        if usuario["PIN"] == pinConsulta and nombreConsulta == usuario["Nombre"]:
+            imprimirInfoUsuario(usuario) #osea le pasa ese en especifico
+            break
+
+        else:
+            print("\nCredenciales incorrectos\n")
+            break
+
+def imprimirInfoUsuario(usuario):
+    print("\nInformación del usuario:")
+    print(usuario)                                                                  
+                                                                  
+cargarUsuarios()
+while True:
+    print("Bienvenido al programa. Seleccione una opcion")
+    opcion = str(input("Seleciione una opción:\n1=Registrar usuario \n2=Ver info de usuario y sus dispositivos\nOtra tecla= Salir\nOPCIÓN:"))
+    if opcion == "1":
+        crearArchivoUsuarios()
+        agregarInfoUsuario()
+        agregarHabitacionDispositivos()
+    elif opcion =="2":
+        consultarInfoPorPIN()
+    else:
+        break
